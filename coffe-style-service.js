ca996a3a14bf99201e1e86e01bca7f3120c83617
Humberto Monaco Palaia <humberto.palaia@mc1global.com>
2022-12-28 01:11:12 -0300

    removido pacote msnodesqlv8, adicionado nome de colunas

--- coffe-style-service.js
+++ coffe-style-service.js
@@ -4,11 +4,11 @@ const dataAccess = require('./data-access');
 module.exports = {
 
   listAll: function (callBack) {        
-    dataAccess.execQuery('SELECT * FROM CoffeStyle', callBack);
+    dataAccess.execQuery('SELECT Id, Name, Active, MinTemperature, MaxTemperature, CreateDate, UpdateDate FROM CoffeStyle', callBack);
   },
   getById : function(id, callBack)
   {
-    dataAccess.execQuery(`SELECT * FROM CoffeStyle WHERE Id = ${id}`, function(result)
+    dataAccess.execQuery(`SELECT Id, Name, Active, MinTemperature, MaxTemperature, CreateDate, UpdateDate FROM CoffeStyle WHERE Id = ${id}`, function(result)
     {
       if(result  && result.length > 0)
         callBack(result[0]);
