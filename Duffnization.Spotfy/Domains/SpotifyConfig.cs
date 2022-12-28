d70d6bca2cd09696cc7bca1e5d1f1d2ad7b60cc2
Humberto Monaco Palaia <humberto.palaia@mc1global.com>
2022-12-28 18:47:19 -0300

    Removido configurações desnecessárias

--- Duffnization.Spotfy/Domains/SpotifyConfig.cs
+++ Duffnization.Spotfy/Domains/SpotifyConfig.cs
@@ -2,9 +2,7 @@
 {
     public class SpotifyConfig
     {
-        public string CallbackUrl { get; set; }
         public string AuthUrl { get; set; }
-        public string AccessTokenUrl { get; set; }
         public string ClientId { get; set; }
         public string ClientSecret { get; set; }
         public string BaseApiUrl { get; set; }
